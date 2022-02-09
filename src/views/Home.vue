fe6c68e02733ec475566ae413d2f15c283b256d1
Dustella <fdnoaivj@outlook.com>
2022-02-09 14:23:22 +0800

    Home 界面完成

---
+++ src/views/Home.vue
@@ -0,0 +1,50 @@
+<template>
+  <nav>
+    <ul v-for="i in links" :key="i.id">
+      <li>
+        <a :href="i.href">{{ i.name }}</a>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "Home",
+  data() {
+    return {
+      links: [
+        {
+          id: 1,
+          name: "博客",
+          href: "https://blog.dustella.net",
+        },
+        {
+          id: 2,
+          name: "朋友们",
+          href: "/friends",
+        },
+        {
+          id: 3,
+          name: "杂货铺",
+          href: "https://index.dustella.net",
+        },
+      ],
+    };
+  },
+});
+</script>
+
+<style scoped>
+nav {
+  display: flex;
+  flex-direction: row;
+}
+@media screen and (max-width: 480px) {
+  nav {
+    flex-direction: column;
+  }
+}
+</style>
