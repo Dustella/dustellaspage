0225b5609b05eb8234c1bfbfb0470b4ecc219c88
Dustella <fdnoaivj@outlook.com>
2022-04-25 13:35:58 +0800

    added lixiang810 to friends list

--- src/views/Friends.vue
+++ src/views/Friends.vue
@@ -11,42 +11,38 @@
   </header>
 </template>
 
-<script lang="ts">
-import { defineComponent } from "vue";
-
-export default defineComponent({
-  name: "Home",
-  data() {
-    return {
-      links: [
-        {
-          id: 1,
-          name: "需要光的残影",
-          href: "https://www.nya.al",
-          description: "残影需要光",
-        },
-        {
-          id: 2,
-          name: "瓜瓜",
-          href: "https://cd.al",
-          description: "瓜瓜好帅啊",
-        },
-        {
-          id: 3,
-          name: "Julian",
-          href: "https://jurt4201.blogspot.com/",
-          description: "膜大佬Julian",
-        },
-        {
-          id: 4,
-          name: "超可爱的瑾知",
-          href: "https://qingwu.top",
-          description: "~超级可爱的瑾知~",
-        },
-      ],
-    };
+<script lang="ts" setup>
+const links = [
+  {
+    id: 1,
+    name: "需要光的残影",
+    href: "https://www.nya.al",
+    description: "残影需要光",
   },
-});
+  {
+    id: 2,
+    name: "瓜瓜",
+    href: "https://cd.al",
+    description: "瓜瓜好帅啊",
+  },{
+    id: 3,
+    name:"lixiang810",
+    href: "https://stblog.penclub.club/",
+    description: "膜lixiang810"
+  },
+  {
+    id: 4,
+    name: "Julian",
+    href: "https://jurt4201.blogspot.com/",
+    description: "啥都玩的Julian",
+  },
+  {
+    id: 5,
+    name: "超可爱的瑾知",
+    href: "https://qingwu.top",
+    description: "~超级可爱的瑾知~",
+  },
+];
 </script>
 
 <style scoped>
@@ -61,7 +57,6 @@ li {
 li > p {
   transform: translateY(0.5rem);
   font-size: 0.8rem;
-
 }
 @media screen and (max-width: 480px) {
   nav {
